c75d9f07581c6ce8e3b8bc7537402c98c8dfadbb
PradatiusD <danielprada2012@gmail.com>
2013-07-18 11:35:37 -0400

    basic icon overlay for contact page

--- Contact-Estrella.php
+++ Contact-Estrella.php
@@ -27,14 +27,41 @@
                         </form>
                 </div>
                 <div class="span6">
-                    <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Estrella+Morena+Flamenco+Dance,+West+Flagler+Street,+Miami,+FL&amp;aq=0&amp;oq=estrella+morena&amp;sll=27.698638,-83.804601&amp;sspn=11.365986,19.753418&amp;t=h&amp;ie=UTF8&amp;hq=Estrella+Morena+Flamenco+Dance,&amp;hnear=W+Flagler+St,+Miami,+Florida&amp;ll=25.768852,-80.332557&amp;spn=0.090433,0.154324&amp;z=13&amp;iwloc=A&amp;cid=7343608998704852238&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Estrella+Morena+Flamenco+Dance,+West+Flagler+Street,+Miami,+FL&amp;aq=0&amp;oq=estrella+morena&amp;sll=27.698638,-83.804601&amp;sspn=11.365986,19.753418&amp;t=h&amp;ie=UTF8&amp;hq=Estrella+Morena+Flamenco+Dance,&amp;hnear=W+Flagler+St,+Miami,+Florida&amp;ll=25.768852,-80.332557&amp;spn=0.090433,0.154324&amp;z=13&amp;iwloc=A&amp;cid=7343608998704852238" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+                    <div id="map-canvas" style="height:200px;"></div>
                     <h3>Estrella Morena Flamenco Dance Studio</h3>
                     <h5>8410 W Flagler St Miami, FL 33144 Suite 115b</h5>
                     <h5>(305) 554-7282</h5>
                     <h5>contact@estrellamorena.com</h5>                
                 </div>
             </section>             
-        </div>       
+        </div>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>    
+    <script>
+    // Enable the visual refresh
+    google.maps.visualRefresh = true;
+
+
+    var map;
+    var estrellaLocation = new google.maps.LatLng(25.768852,-80.332557);
+    function initialize() {
+      var mapOptions = {
+        zoom: 15,
+        center: estrellaLocation,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: estrellaLocation,
+            map:map,
+            title:"Hello World!"
+        });
+    }
+
+
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    </script>       
     </body>
 </html>
 
